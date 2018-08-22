f4bdcc0a54ccab9dd325be2f04dc9396212c55e9
Orivaldo Santana <orivaldo@gmail.com>
2018-08-22 15:33:15 -0300

    Novos códigos de exemplos

---
+++ codigos/parte4/C/ect.js
@@ -0,0 +1,22 @@
+function lerTexto(x) {
+	return prompt(x);
+}
+function escreva(x) {
+	return alert(x);
+}
+function lerInteiro(x) {
+	return parseInt(prompt(x));
+}
+function lerReal(x) {
+	return parseFloat(prompt(x));
+}
+function raizQuadrada(x){
+	return Math.sqrt(x);
+}
+function potencia(b,e){
+	return Math.pow(b,e); 
+}
+function divisaoInteira(x,y) {
+	return Math.floor(x/y);
+}
+
---
+++ codigos/parte5/D/ect.js
@@ -0,0 +1,25 @@
+function lerTexto(x) {
+	return prompt(x);
+}
+function escreva(x) {
+	return alert(x);
+}
+function lerInteiro(x) {
+	return parseInt(prompt(x));
+}
+function lerReal(x) {
+	return parseFloat(prompt(x));
+}
+function raizQuadrada(x){
+	return Math.sqrt(x);
+}
+function potencia(b,e){
+	return Math.pow(b,e); 
+}
+function divisaoInteira(x,y) {
+	return Math.floor(x/y);
+}
+function piso(x) {
+	return Math.floor(x)
+}
+
